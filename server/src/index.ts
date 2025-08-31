1c97fb1e602eb0e1020d63720ae30d850c8e5902
Felipe Messias <felipe.messias.fms@gmail.com>
2025-08-31 13:24:57 -0300

    fix: fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const authHandler = toNodeHandler(auth);
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:4173",
+  origin: "https://senne-liquor.vercel.app",
   credentials: true
 }));
 app.use(express.json());
